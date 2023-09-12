76d4a5b593029b128d9222a74f2c711480c038e5
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-09-12 15:04:36 +0530

    First commit

---
+++ NunitTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTest
+{
+    internal class Program
+    {
+ public static string SignIn(string userName, string userPwd)
+        {
+            string msg;
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(userPwd))
+            {
+                msg = "user name and password must not be null";
+
+            }
+            else
+            {
+                if ((userName == "sam") && (userPwd == "sam@1256@1256"))
+                {
+                    msg = "SignIn success";
+
+                }
+                else
+                {
+                    msg = "Invalid Credential";
+                }
+            }
+            return msg;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ NunitTest/UserAuthenticator.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTest
+{
+    [TestFixture]
+    internal class UserAuthenticator
+    {
+        [Test]
+        public void SigninTest()
+        {
+            string empResult = "user name and password must not be null";
+            string failResult = "Invalid Credential";
+            string sResult = "SignIn success";
+            Assert.AreEqual(empResult, Program.SignIn(null, null));
+            Assert.AreEqual(empResult, Program.SignIn("", ""));
+            Assert.AreEqual(failResult, Program.SignIn("ravi", "ravi@123"));
+            Assert.AreEqual(sResult, Program.SignIn("sam", "sam@1256@1256"));
+
+        }
+    }
+}
